17a83528d8b002d750079e583d8032346e6d2283
sathvikmaroju <sathvik18052002@gmail.com>
2022-02-09 15:27:59 +0530

    v0

---
+++ 468 Face Landmarks.py
@@ -0,0 +1,20 @@
+import cv2
+import mediapipe as mp 
+
+cap = cv2.VideoCapture(0)
+pTime=0
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=1)
+drawSpec = mpDraw.DrawingSpec(thickness=1,circle_radius=2) # you can change these values to play..
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION, drawSpec,drawSpec)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
